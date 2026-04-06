92aaa103e4463d34b8593c15d61cdb07a26a0c0c
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-04-06 20:54:27 -0300

    update base dao

--- src/main/java/model/DAO/BaseDAO.java
+++ src/main/java/model/DAO/BaseDAO.java
@@ -1,6 +1,7 @@
 package model.DAO;
 
 import java.util.List;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
@@ -16,7 +17,16 @@ public abstract class BaseDAO<T> implements InterfaceDAO<T> {
 
     @Override
     public void Create(T objeto) {
-        em.persist(objeto);
+        em.getTransaction().begin();
+        try {
+            em.persist(objeto);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        }
     }
 
     @Override
@@ -26,8 +36,8 @@ public abstract class BaseDAO<T> implements InterfaceDAO<T> {
 
     @Override
     public List<T> Retrieve(String atributo, String valor) {
-        String jpql = "SELECT o FROM " + classeEntidade.getSimpleName() + 
-                      " o WHERE o." + atributo + " LIKE :valor";
+        String jpql = "SELECT o FROM " + classeEntidade.getSimpleName()
+                + " o WHERE o." + atributo + " LIKE :valor";
         TypedQuery<T> query = em.createQuery(jpql, classeEntidade);
         query.setParameter("valor", "%" + valor + "%");
         return query.getResultList();
@@ -35,24 +45,52 @@ public abstract class BaseDAO<T> implements InterfaceDAO<T> {
 
     @Override
     public void Update(T objeto) {
-        em.merge(objeto);
+        em.getTransaction().begin();
+        try {
+            em.merge(objeto);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        }
     }
 
     @Override
     public void Delete(T objeto) {
-        T objetoGerenciado = em.contains(objeto) ? objeto : em.merge(objeto);
-        em.remove(objetoGerenciado);
+        em.getTransaction().begin();
+        try {
+            T objetoGerenciado = em.contains(objeto) ? objeto : em.merge(objeto);
+            em.remove(objetoGerenciado);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        }
     }
 
     @Override
     public void AtivarInativar(int id, boolean ativar) {
         char status = ativar ? 'A' : 'I';
-        String jpql = "UPDATE " + classeEntidade.getSimpleName() + 
-                      " o SET o.status = :status WHERE o.id = :id";
-        em.createQuery(jpql)
-          .setParameter("status", status)
-          .setParameter("id", id)
-          .executeUpdate();
+        String jpql = "UPDATE " + classeEntidade.getSimpleName()
+                + " o SET o.status = :status WHERE o.id = :id";
+
+        em.getTransaction().begin();
+        try {
+            em.createQuery(jpql)
+                    .setParameter("status", status)
+                    .setParameter("id", id)
+                    .executeUpdate();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        }
     }
 
     public List<T> listarTodos() {
